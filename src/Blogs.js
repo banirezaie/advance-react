a0e39ba00bd670a76f61e5faabb9b4c400aa257d
BANI GIT <bani.rezaie@gmail.com>
2021-03-08 09:50:04 +0000

    delete post

--- src/Blogs.js
+++ src/Blogs.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 
 const Blog = () => {
@@ -6,24 +7,20 @@ const Blog = () => {
         {title: 'first blog is mine', body: 'lorem ipsum...', author: 'mario', id: 1},
         {title: 'second blog is yours', body: 'lorem ipsum....', author: 'yoshi', id: 2},
         {title: 'third blog is he/him', body: 'lorem ipsum...', author: 'luigi', id: 3},
-        {title: 'fourth blog is she/her', body: 'lorem ipsum...', author: 'ninja', id: 4}
+        {title: 'fourth blog is she/her', body: 'lorem ipsum...', author: 'mario', id: 4}
     ])
+
+const handleDelete = (index)=>{
+    const newBlogs = blogs.filter(blog => blogs.indexOf(blog) !== index)
+    setBlogs(newBlogs)
+}
+
+
     return ( 
-        <div className="blog">{
-            blogs.map((post, i)=>{
-                return (
-                  <div className='post' key={i}>
-                    <h3>{post.title}</h3>
-                    <span>
-                       written by {post.author}
-                    </span>
-                    <p>
-                        {post.body}
-                    </p>
-                  </div>
-                );
-            })
-        }</div>
+        <div className="blog">
+            <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+            <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's blogs" />
+        </div>
      );
 }
  
